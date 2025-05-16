da2cb77cdfa6538f7bf10b3bf2cc8fde86c552da
Yodaman07 <ayaan.irshad@gmail.com>
2025-05-16 11:10:07 -0700

    Added starter code

---
+++ Mario2D/src/Frame.java
@@ -0,0 +1,64 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Frame extends JPanel implements ActionListener, KeyListener{
+	public static int width = 500;
+	public static int height = 500;
+	
+	
+	public Frame() {
+		JFrame f = new JFrame("Game");
+		f.setSize(width, height);
+		f.setBackground(Color.white);
+		f.setResizable(true);
+		f.addKeyListener(this);
+		
+		
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame frame = new Frame();
+		System.out.println("hello world");
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
