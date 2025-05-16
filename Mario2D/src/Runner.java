4dc480ba0f31a0c128b908f5d668d8a0f752aae5
tkngu <tkngu@10.9.137.76>
2025-05-16 10:59:39 -0700

    yes

--- Mario2D/src/Runner.java
+++ Mario2D/src/Runner.java
@@ -4,6 +4,6 @@ public class Runner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-	}
+		System.out.println("Daddy Irshad Jr");	}
 
 }
